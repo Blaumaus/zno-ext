6676dfe706897678054480fafef01bf6b6379b71
ookldev <margareq@protonmail.ch>
2019-05-27 17:25:11 +0300

    Popup display bugfix

--- src/js/background.js
+++ src/js/background.js
@@ -2,3 +2,13 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ show: false }, () => {})
 })
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+  chrome.declarativeContent.onPageChanged.addRules([{
+    conditions: [new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: { hostEquals: 'zno.osvita.ua' },
+    })
+    ],
+    actions: [new chrome.declarativeContent.ShowPageAction()]
+  }])
+})
